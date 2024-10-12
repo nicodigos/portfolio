92b7c83930b9b111521b9ba54ecf3073feb17c52
Nicolas Peralta <nperaltapz@gmail.com>
2024-10-12 11:47:42 -0400

    SectionContent and SectionTitle responsive

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -7,6 +7,7 @@ function Introduction() {
   imageDescriptionArray.push(
     <ImageDescription
       image={"/public/images/rocket.svg"}
+      key={"/public/images/rocket.svg"}
       title={"Value Creation"}
       description={
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean rutrum velit nunc, et porta dui pellentesque vel. Sed consequat vehicula maximus. Cras vehicula massa auctor, elementum urna in, laoreet nisi. Phasellus consectetur sem vel eros efficitur, id ornare massa ultrices. Aliquam erat volutpat"
@@ -14,26 +15,31 @@ function Introduction() {
     ></ImageDescription>,
     <ImageDescription
       image={"/public/images/analysis.svg"}
+      key={"/public/images/analysis.svg"}
       title={"Visualization"}
       description={"This is an awsome description"}
     ></ImageDescription>,
     <ImageDescription
       image={"/public/images/api.svg"}
+      key={"/public/images/api.svg"}
       title={"Data Engineering"}
       description={"This is an awsome description"}
     ></ImageDescription>,
     <ImageDescription
       image={"/public/images/webdev.svg"}
+      key={"/public/images/webdev.svg"}
       title={"Web Development"}
       description={"This is an awsome description"}
     ></ImageDescription>,
     <ImageDescription
       image={"/public/images/security.svg"}
+      key={"/public/images/security.svg"}
       title={"Data Security"}
       description={"This is an awsome description"}
     ></ImageDescription>,
     <ImageDescription
       image={"/public/images/apple.svg"}
+      key={"/public/images/apple.svg"}
       title={"Outside the box"}
       description={"This is an awsome description"}
     ></ImageDescription>
