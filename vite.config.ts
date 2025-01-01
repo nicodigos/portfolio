7e0e2ca4f8446bb0be67f519b6d7fbc799aa9abb
Nicolas Peralta <nperaltapz@gmail.com>
2025-01-01 11:27:11 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,6 @@ import react from "@vitejs/plugin-react";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-  base: "/react-portf",
-  plugins: [
-    react(),
-    // createHtmlPlugin({
-    //   minify: true, // Minify the HTML for production
-    // }),
-  ],
-  // build: {
-  //   rollupOptions: {
-  //     input: "./index.html",
-  //     output: {
-  //       manualChunks: undefined,
-  //     },
-  //   },
-  // },
+  plugins: [react()],
+  base: "{react-portf}",
 });
